bc7d102465a2dd4308ebe56caffcd4feb50e986f
OrskiTorski <oscar.tafvelin@elev.ga.ntig.se>
2024-09-10 11:04:33 +0200

    gjort grejen

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express" // ESM
+import nunjucks from "nunjucks"
+const app = express()
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+})
+app.use(express.static("public"))
+app.get("/", (request, response) => {
+  response.render("index.njk", {
+    message: "Hemsidan nunjuckad och klar weee!",
+    title: "Hem",
+    url: "https://github.com/Orskitorski",
+  })
+})
+app.get("/om", (request, response) => {
+  response.render("om.njk", {
+    message: "Omsidan!",
+    title: "Om",
+  })
+})
+
+
+app.get("/readme", (request, response) => {
+  console.log(request)
+  response.json({
+    message: "Hello World",
+  })
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
